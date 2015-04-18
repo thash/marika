501eb9f9ed103c3bd9ec7933545cda6209fcb3ee
memerelics <takuya21hashimoto@gmail.com>
2015-04-18 15:03:23 +0900

    deploy to navi-s

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,14 +1,14 @@
 # config valid only for current version of Capistrano
 lock '3.4.0'
 
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :application, 'marika'
+set :repo_url, 'git@github.com:memerelics/marika.git'
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, '/var/www/my_app_name'
+set :deploy_to, '/home/hash/deploy/marika'
 
 # Default value for :scm is :git
 # set :scm, :git
